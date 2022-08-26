b2bf8b0f8d4095a14c20ec74db7f2bd323ab5902
Mariam-Elbana <marimel.bana44@gmail.com>
2022-08-26 11:56:55 +0200

    Assignment 1

---
+++ Week 4/Assignment 1/main.js
@@ -0,0 +1,18 @@
+// التكليف 01
+
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(10**5);      // 100000
+console.log(1e5);        // 100000
+console.log(200000 / 2); // 100000
+console.log(2e5 / 2);    // 100000
+console.log(10_0000);    // 100000
+console.log(10e4);       // 100000
+console.log(1000 * 100); // 100000
+console.log(1e4 + 9e4);  // 100000
+console.log(100e3);      // 100000
+console.log(2e5 - 1e5);  // 100000
