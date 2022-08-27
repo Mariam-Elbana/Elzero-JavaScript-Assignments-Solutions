af9c964c9eb377f92d36f5321d933ae89c49ab57
Mariam-Elbana <marimel.bana44@gmail.com>
2022-08-27 15:33:48 +0200

    Assignment 3

---
+++ Week 5/Assignment 3/main.js
@@ -0,0 +1,11 @@
+// تكليفات JavaScript Bootcamp من الدرس 031 إلى 032
+
+// التكليف 03
+
+let a = 20;
+let b = 30;
+let c = 10;
+
+console.log((a < b && a > c) || a < b); // true
+console.log(a != b || a > c); // true
+console.log(!(a > b) && !(a === b) && !(a < c) && !(a == c)); // true
