0586e9ac2ea89cfc8f2f12b2d1e4731eb0eb34d9
Mariam-Elbana <marimel.bana44@gmail.com>
2022-08-27 14:38:14 +0200

    Challenge Solution

---
+++ Week 5/Challenges/challenge2.js
@@ -0,0 +1,11 @@
+// Challenge Lesson 37
+
+let a = 10;
+
+a < 10
+  ? console.log("10")
+  : a >= 10 && a <= 40
+  ? console.log("10 to 40")
+  : a > 40
+  ? console.log(">40")
+  : console.log("unknown");
