f661345e51276bbec65fb7229c57b7daa6c2be94
Mariam-Elbana <marimel.bana44@gmail.com>
2022-08-27 15:27:27 +0200

    Assignment 2

---
+++ Week 5/Assignment 2/main.js
@@ -0,0 +1,13 @@
+// تكليفات JavaScript Bootcamp من الدرس 031 إلى 032
+
+// التكليف 02
+
+let num1 = 10;
+let num2 = 20;
+
+console.log(num1 < num2); // true
+console.log(num2 > num1); // true
+console.log(num1 !== num2); // true
+console.log(num1 != num2); // true
+console.log(typeof num1 === typeof num2); // true
+console.log(Number.isInteger(num1, num2)); // true
