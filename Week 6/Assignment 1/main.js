432df8b2bac220056a059896c8b47841ac8e5ee3
Mariam-Elbana <marimel.bana44@gmail.com>
2022-08-29 18:23:33 +0200

    Assignment 1

---
+++ Week 6/Assignment 1/main.js
@@ -0,0 +1,10 @@
+// التكليف 01
+
+let myFriends = ["Ahmed", "Elham", "Osama", "Gamal"];
+let num13 = 3;
+
+// Method 1
+console.log(myFriends.slice(0, 3)); // ["Ahmed", "Elham", "Osama"];
+
+// Method 2
+console.log(myFriends.slice(-4, -1)); // ["Ahmed", "Elham", "Osama"];
