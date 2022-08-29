31b1529b0fbaf2e2827f7951694408ab734b8e4f
Mariam-Elbana <marimel.bana44@gmail.com>
2022-08-29 18:35:17 +0200

    Assignment 4

---
+++ Week 6/Assignment 4/main.js
@@ -0,0 +1,9 @@
+// التكليف 04
+
+let website = "Go";
+let words = [`${website}ogle`, "Facebook", ["Elzero", "Web", "School"]];
+let zeroo = 0;
+
+console.log(
+  words[++zeroo + zeroo][--zeroo].slice(++zeroo + zeroo).toUpperCase()
+); // ZERO
