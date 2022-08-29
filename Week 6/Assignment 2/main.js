592469fef1807519951f73eebcb94b09d4577eff
Mariam-Elbana <marimel.bana44@gmail.com>
2022-08-29 18:28:21 +0200

    Assignment 2

---
+++ Week 6/Assignment 2/main.js
@@ -0,0 +1,9 @@
+// التكليف 02
+
+let friends = ["Ahmed", "Eman", "Osama", "Gamal"];
+
+// Write Your Code Here
+
+console.log(friends.pop());
+console.log(friends.shift());
+console.log(friends); // ["Eman", "Osama"]
