ba477f713fc27df745806a2a0bfeea23296fa391
Mariam-Elbana <marimel.bana44@gmail.com>
2022-08-29 18:48:44 +0200

    Assignment 6

---
+++ Week 6/Assignment 6/main.js
@@ -0,0 +1,8 @@
+// التكليف 06
+
+let arr1 = ["A", "C", "X"];
+let arr2 = ["D", "E", "F", "Y"];
+let allArrs = [arr1.concat(arr2).sort().slice(arr2.length)];
+
+// Your Code Here
+console.log(allArrs.toString().toLowerCase()); // fxy
