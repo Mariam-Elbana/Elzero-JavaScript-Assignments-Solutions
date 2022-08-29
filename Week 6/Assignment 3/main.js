67d4478176a258c5414efa9846b98889b36fb370
Mariam-Elbana <marimel.bana44@gmail.com>
2022-08-29 18:30:52 +0200

    Assignment 3

---
+++ Week 6/Assignment 3/main.js
@@ -0,0 +1,9 @@
+// التكليف 03
+
+let arrOne = ["C", "D", "X"];
+let arrTwo = ["A", "B", "Z"];
+let finalArr = [arrTwo.concat(arrOne).sort().reverse()];
+
+// Write One Single Line Of Code
+
+console.log(finalArr); // ["Z", "X", "D", "C", "B", "A"]
