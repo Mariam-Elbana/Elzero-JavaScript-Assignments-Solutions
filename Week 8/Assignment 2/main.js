c72ed1741a1789423d5ca0826f9fb644d542eaf4
Mariam-Elbana <marimel.bana44@gmail.com>
2022-08-30 20:45:00 +0200

    Assignment 2

---
+++ Week 8/Assignment 2/main.js
@@ -0,0 +1,24 @@
+// Assignments تكليفات JavaScript Bootcamp من الدرس 057 إلى 063
+
+// التكليف 02
+
+function calculate(firstNum, secondNum, operation) {
+  if (secondNum === undefined) {
+    console.log(`Second Number Not Found`);
+  } else if (operation === undefined) {
+    console.log(firstNum + secondNum);
+  } else if (operation === "add") {
+    console.log(firstNum + secondNum);
+  } else if (operation === "subtract") {
+    console.log(firstNum - secondNum);
+  } else if (operation === "multiply") {
+    console.log(firstNum * secondNum);
+  }
+}
+
+// Needed Output
+calculate(20); // Second Number Not Found
+calculate(20, 30); // 50
+calculate(20, 30, "add"); // 50
+calculate(20, 30, "subtract"); // -10
+calculate(20, 30, "multiply"); // 600
