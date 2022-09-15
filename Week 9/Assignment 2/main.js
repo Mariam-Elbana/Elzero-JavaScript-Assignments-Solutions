bb55e17ecd515a798e5f5c213e5d8016dc6cf1d7
Mariam-Elbana <marimel.bana44@gmail.com>
2022-09-15 12:47:13 +0200

    Assignment 2

---
+++ Week 9/Assignment 2/main.js
@@ -0,0 +1,15 @@
+// تكليفات JavaScript Bootcamp من الدرس 064 إلى 070
+
+// التكليف 02
+
+// First Function
+
+let itsMe = () => `Iam A Normal Function`;
+
+console.log(itsMe()); // Iam A Normal Function
+
+// Second Function
+
+let urlCreate = (protocol, web, tld) => `${protocol}://www.${web}.${tld}`;
+
+console.log(urlCreate("https", "elzero", "org")); // https://www.elzero.org
