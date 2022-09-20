861563a66d8629f94573f947ffded6bad7317902
Mariam-Elbana <marimel.bana44@gmail.com>
2022-09-20 15:48:49 +0200

    Assignment 1

---
+++ Week 12/Assignment 1/main.js
@@ -0,0 +1,35 @@
+//تكليفات JavaScript Bootcamp من الدرس 086 إلى 093
+
+// التكليف 01
+
+let myDiv1 = document.getElementById("elzero");
+let myDiv2 = document.getElementsByClassName("element")[0];
+let myDiv3 = document.getElementsByTagName("div")[0];
+let myDiv4 = document.getElementsByName("js")[0];
+let myDiv5 = document.querySelector("div");
+let myDiv6 = document.querySelector("[name = 'js']");
+let myDiv7 = document.querySelector("[id]");
+let myDiv8 = document.querySelector("div:first-of-type");
+let myDiv9 = document.querySelectorAll("div.element")[0];
+let myDiv10 = document.querySelectorAll("div#elzero")[0];
+let myDiv11 = document.querySelectorAll("div")[0];
+let myDiv12 = document.querySelectorAll("[name = 'js']")[0];
+let myDiv13 = document.body.children[0];
+let myDiv14 = document.body.firstElementChild;
+let myDiv15 = document.body.childNodes[1];
+
+console.log(myDiv1);
+console.log(myDiv2);
+console.log(myDiv3);
+console.log(myDiv4);
+console.log(myDiv5);
+console.log(myDiv6);
+console.log(myDiv7);
+console.log(myDiv8);
+console.log(myDiv9);
+console.log(myDiv10);
+console.log(myDiv11);
+console.log(myDiv12);
+console.log(myDiv13);
+console.log(myDiv14);
+console.log(myDiv15);
