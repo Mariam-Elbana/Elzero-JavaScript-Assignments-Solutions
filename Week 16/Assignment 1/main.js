fc0ebfa23abfbaecd4bf8ce5f49df75ad22bce0b
Mariam-Elbana <marimel.bana44@gmail.com>
2022-09-27 10:49:20 +0200

    Assignment 1

---
+++ Week 16/Assignment 1/main.js
@@ -0,0 +1,10 @@
+// تكليفات JavaScript Bootcamp من الدرس 115 إلى 122
+
+// التكليف 01
+
+let myNumbers = [1, 2, 3, 4, 5];
+
+// Write Your Destructuring Assignment Here
+let [a, b, c, d, e] = myNumbers;
+
+console.log(a * e); // 5
