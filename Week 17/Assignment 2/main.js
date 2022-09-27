a463e4cbfdcc825f32f8d8f9d30560b349dda5b5
Mariam-Elbana <marimel.bana44@gmail.com>
2022-09-27 13:53:24 +0200

    Assignment 2

---
+++ Week 17/Assignment 2/main.js
@@ -0,0 +1,8 @@
+//تكليفات JavaScript Bootcamp من الدرس 123 إلى 133
+
+// التكليف 02
+
+let myFriends = ["Osama", "Ahmed", "Sayed", "Sayed", "Mahmoud", "Osama"];
+
+console.log(new Set(myFriends.sort()));
+// (4) ['Ahmed', 'Mahmoud', 'Osama', 'Sayed']
