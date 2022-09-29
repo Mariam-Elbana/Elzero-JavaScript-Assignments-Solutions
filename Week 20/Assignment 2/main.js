3b67b5814f76f084c238e1203e23890408ad61fa
Mariam-Elbana <marimel.bana44@gmail.com>
2022-09-29 18:17:31 +0200

    Assignment 2

---
+++ Week 20/Assignment 2/main.js
@@ -0,0 +1,10 @@
+// تكليفات JavaScript Bootcamp من الدرس 159 إلى 168
+
+// التكليف 02
+
+let dateAndTime = new Date(1980, 0, 1, 0, 0, 0, 1000);
+
+console.log(dateAndTime);
+
+// Needed Output
+// "Tue Jan 01 1980 00:00:01 GMT+0200 (Eastern European Standard Time)"
